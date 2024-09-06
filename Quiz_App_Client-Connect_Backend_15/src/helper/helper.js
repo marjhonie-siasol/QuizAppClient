9bb0c6af00e54cded618319ff44172fadae19bc4
Marjhonie <157345383+marjhonie-siasol@users.noreply.github.com>
2024-09-07 02:25:45 +0800

    Update helper.js

--- Quiz_App_Client-Connect_Backend_15/src/helper/helper.js
+++ Quiz_App_Client-Connect_Backend_15/src/helper/helper.js
@@ -6,6 +6,7 @@ export function attempts_Number(result){
     return result.filter(r => r !== undefined).length;
 }
 
+
 export function earnPoints_Number(result, answers, point){
     return result.map((element, i) => answers[i] === element).filter(i => i).map(i => point).reduce((prev, curr) => prev + curr, 0);
 }
